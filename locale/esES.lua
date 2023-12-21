3d9cec1fc371ad53978e8e0ea3e89165b1d6130e
D4KiR <23246564+d4kir92@users.noreply.github.com>
2023-12-21 15:58:42 +0100

    v0.6.2

---
+++ locale/esES.lua
@@ -0,0 +1,11 @@
+-- esES Spanish
+local _, _ = ...
+D4:AddTrans("esES", "LID_GENERAL", "General")
+D4:AddTrans("esES", "LID_SHOWMINIMAPBUTTON", "Botón Mostrar minimapa")
+D4:AddTrans("esES", "LID_SHOWOTHERSPECS", "Mostrar otras especificaciones")
+D4:AddTrans("esES", "LID_SHOWOTHERCLASSES", "Mostrar otras Clases")
+D4:AddTrans("esES", "LID_YOURSPEC", "Tu especialización")
+D4:AddTrans("esES", "LID_OTHERSPECS", "Otras especialización")
+D4:AddTrans("esES", "LID_OTHERCLASSES", "Otras Clases")
+D4:AddTrans("esES", "LID_NOOTHERCLASSNEEDSTHIS", "Ninguna otra Clase necesita esto")
+D4:AddTrans("esES", "LID_NOOTHERSPECNEEDSTHIS", "Ninguna otra especialización necesita esto")

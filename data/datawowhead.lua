010708e2a5d751eaf53c191a61e37e2f87061900
D4KiR <23246564+d4kir92@users.noreply.github.com>
2024-07-04 18:27:04 +0200

    v0.10.21

--- data/datawowhead.lua
+++ data/datawowhead.lua
@@ -1,7 +1,7 @@
 -- By D4KiR
 local _, SpecBisTooltip = ...
 local BIS = {}
--- DATA FROM: 26.06.2024
+-- DATA FROM: 04.07.2024
 if SpecBisTooltip:GetWoWBuild() == "RETAIL" then
 	BIS["RETAIL"] = {}
 	BIS["RETAIL"]["DEATHKNIGHT"] = {}

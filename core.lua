9a8e53e78269bbeb9323056ac9d13dc4fc99df6e
D4KiR <23246564+d4kir92@users.noreply.github.com>
2025-02-14 12:38:23 +0100

    v0.12.14

--- core.lua
+++ core.lua
@@ -21,7 +21,7 @@ SBTSetup:SetScript(
 					["icon"] = 136031,
 					["var"] = mmbtn,
 					["dbtab"] = SBTTAB,
-					["vTT"] = {{"SpecBisTooltip |T136031:16:16:0:0|t", "v|cff3FC7EB0.12.13"}, {"Leftclick", "Open Settings"}, {"Rightclick", "Hide Minimap Icon"}},
+					["vTT"] = {{"SpecBisTooltip |T136031:16:16:0:0|t", "v|cff3FC7EB0.12.14"}, {"Leftclick", "Open Settings"}, {"Rightclick", "Hide Minimap Icon"}},
 					["funcL"] = function()
 						SpecBisTooltip:ToggleSettings()
 					end,
@@ -92,14 +92,14 @@ end
 
 function SpecBisTooltip:InitSettings()
 	SBTTAB = SBTTAB or {}
-	SpecBisTooltip:SetVersion(AddonName, 136031, "0.12.13")
+	SpecBisTooltip:SetVersion(AddonName, 136031, "0.12.14")
 	sbt_settings = SpecBisTooltip:CreateFrame(
 		{
 			["name"] = "SpecBisTooltip",
 			["pTab"] = {"CENTER"},
 			["sw"] = 520,
 			["sh"] = 520,
-			["title"] = format("SpecBisTooltip |T136031:16:16:0:0|t v|cff3FC7EB%s", "0.12.13")
+			["title"] = format("SpecBisTooltip |T136031:16:16:0:0|t v|cff3FC7EB%s", "0.12.14")
 		}
 	)
 
@@ -1026,20 +1026,25 @@ local function OnTooltipSetItem(tooltip, data)
 	end
 end
 
-if ItemRefTooltip and GameTooltip and ItemRefTooltip:HasScript("OnTooltipSetItem") and GameTooltip:HasScript("OnTooltipSetItem") then
-	ItemRefTooltip:HookScript(
-		"OnTooltipSetItem",
-		function(tooltip, ...)
-			OnTooltipSetItem(tooltip, ...)
-		end
-	)
+C_Timer.After(
+	4,
+	function()
+		if ItemRefTooltip and GameTooltip and ItemRefTooltip:HasScript("OnTooltipSetItem") and GameTooltip:HasScript("OnTooltipSetItem") then
+			ItemRefTooltip:HookScript(
+				"OnTooltipSetItem",
+				function(tooltip, ...)
+					OnTooltipSetItem(tooltip, ...)
+				end
+			)
 
-	GameTooltip:HookScript(
-		"OnTooltipSetItem",
-		function(tooltip, ...)
-			OnTooltipSetItem(tooltip, ...)
+			GameTooltip:HookScript(
+				"OnTooltipSetItem",
+				function(tooltip, ...)
+					OnTooltipSetItem(tooltip, ...)
+				end
+			)
+		elseif TooltipDataProcessor then
+			TooltipDataProcessor.AddTooltipPostCall(Enum.TooltipDataType.Item, OnTooltipSetItem)
 		end
-	)
-elseif TooltipDataProcessor then
-	TooltipDataProcessor.AddTooltipPostCall(Enum.TooltipDataType.Item, OnTooltipSetItem)
-end
+	end
+)

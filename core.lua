0bb0abd27aa12e6278fc6415de4f37365121bd69
D4KiR <23246564+d4kir92@users.noreply.github.com>
2025-03-02 20:51:33 +0100

    v0.12.27

--- core.lua
+++ core.lua
@@ -14,7 +14,7 @@ SBTSetup:SetScript(
 		if event == "PLAYER_LOGIN" then
 			SBTTAB = SBTTAB or {}
 			SBTTABPC = SBTTABPC or {}
-			SpecBisTooltip:SetVersion(136031, "0.12.26")
+			SpecBisTooltip:SetVersion(136031, "0.12.27")
 			SpecBisTooltip:AddSlash("sbt", SpecBisTooltip.ToggleSettings)
 			SpecBisTooltip:AddSlash("specbistooltip", SpecBisTooltip.ToggleSettings)
 			local mmbtn = nil
--- libs/D4Lib/D4Translations.lua
+++ libs/D4Lib/D4Translations.lua
@@ -1,4 +1,5 @@
 local _, D4 = ...
+local missingTranslations = {}
 function D4:Trans(key, lang, t1, t2, t3)
     D4.trans = D4.trans or {}
     if lang == nil then
@@ -12,7 +13,8 @@ function D4:Trans(key, lang, t1, t2, t3)
     elseif D4.trans["enUS"] and D4.trans["enUS"][key] ~= nil then
         result = D4.trans["enUS"][key]
     else
-        if key ~= "" then
+        if key ~= "" and missingTranslations[key] == nil then
+            missingTranslations[key] = true
             D4:MSG("MISSING TRANSLATION KEY:", key, lang, t1, t2, t3)
         end
 

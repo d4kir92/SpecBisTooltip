3d0a013ed355eae2db984099aa98321fbd2994d4
D4KiR <23246564+d4kir92@users.noreply.github.com>
2025-03-13 11:16:41 +0100

    v0.12.40

--- data/datasources-retail.lua
+++ data/datasources-retail.lua
@@ -7,7 +7,7 @@ if SpecBisTooltip:GetWoWBuild() == "RETAIL" then
     end
 end
 
--- SOURCE FROM: 10.03.2025
+-- SOURCE FROM: 13.03.2025
 if SpecBisTooltip:GetWoWBuild() == "RETAIL" then
     function SpecBisTooltip:TranslationenUS()
         s["npc;drop=224552"] = {"Rasha'nan", "Nerub-ar Palace"}

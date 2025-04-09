40838bae7f73678792c4aa02c2111d66ed0ca233
D4KiR <23246564+d4kir92@users.noreply.github.com>
2025-04-09 22:14:14 +0200

    v0.12.54

--- core.lua
+++ core.lua
@@ -1131,7 +1131,7 @@ SBTSetup:SetScript(
 		if event == "PLAYER_LOGIN" then
 			SBTTAB = SBTTAB or {}
 			SBTTABPC = SBTTABPC or {}
-			SpecBisTooltip:SetVersion(136031, "0.12.53")
+			SpecBisTooltip:SetVersion(136031, "0.12.54")
 			SpecBisTooltip:AddSlash("sbt", SpecBisTooltip.ToggleSettings)
 			SpecBisTooltip:AddSlash("specbistooltip", SpecBisTooltip.ToggleSettings)
 			local mmbtn = nil

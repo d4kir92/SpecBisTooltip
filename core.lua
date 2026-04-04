84b2fccb6e49b2852e063a3b09bc1a6fac2efc24
D4KiR <23246564+d4kir92@users.noreply.github.com>
2026-04-04 03:00:56 +0200

    v0.13.43

--- core.lua
+++ core.lua
@@ -1086,7 +1086,7 @@ SBTSetup:SetScript(
 			SBTTAB = SBTTAB or {}
 			SBTTABPC = SBTTABPC or {}
 			SpecBisTooltip:SetDbTab(SBTTAB)
-			SpecBisTooltip:SetVersion(136031, "0.13.42")
+			SpecBisTooltip:SetVersion(136031, "0.13.43")
 			SpecBisTooltip:AddSlash("sbt", SpecBisTooltip.ToggleSettings)
 			SpecBisTooltip:AddSlash("specbistooltip", SpecBisTooltip.ToggleSettings)
 			local mmbtn = nil

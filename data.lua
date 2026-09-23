a7b91d371ea7109d0d2b5e983bfdfc8a833eec4f
D4KiR <23246564+d4kir92@users.noreply.github.com>
2026-09-23 17:02:32 +0200

    v1.0.8

--- data.lua
+++ data.lua
@@ -2,16 +2,17 @@
 local _, SpecBisTooltip = ...
 SpecBisTooltip:SetAddonOutput("SpecBisTooltip", 136031)
 function SpecBisTooltip:GetBisPool()
-	if SpecBisTooltip:IsCamelot() then return "CLASSIC" end
+	if SpecBisTooltip:IsForever() then return "CLASSIC" end
 	return SpecBisTooltip:GetWoWBuild()
 end
 
-if SpecBisTooltip:IsCamelot() then
+if SpecBisTooltip:IsForever() then
 	local camelotDruidSpecs = {
 		[1] = 1,
 		[2] = 3,
 		[3] = 4,
 	}
+
 	local getTalentInfo = SpecBisTooltip.GetTalentInfo
 	function SpecBisTooltip:GetTalentInfo()
 		local specId, icon = getTalentInfo(self)
